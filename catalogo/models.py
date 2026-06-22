ec1c0b0ba3f1fcf51a5d2f61700a176fa76fdfc6
Enrique Diaz <ospisebas89@gmail.com>
2026-06-22 00:41:04 -0500

    Codigo barras productos

--- catalogo/models.py
+++ catalogo/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Producto(models.Model):
 
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(
+        max_length=200
+    )
 
     marca = models.CharField(
         max_length=100,
@@ -13,6 +15,12 @@ class Producto(models.Model):
     referencia = models.CharField(
         max_length=100,
         unique=True
+    )
+
+    codigo_barras = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True
     )
 
     precio_compra = models.DecimalField(
@@ -40,9 +48,44 @@ class Producto(models.Model):
         auto_now_add=True
     )
 
+    def save(self, *args, **kwargs):
+
+        if not self.codigo_barras:
+
+            ultimo = Producto.objects.order_by(
+                '-id'
+            ).first()
+
+            siguiente = 100000
+
+            if ultimo and ultimo.codigo_barras:
+
+                try:
+
+                    siguiente = (
+                        int(
+                            ultimo.codigo_barras.replace(
+                                'CS',
+                                ''
+                            )
+                        ) + 1
+                    )
+
+                except:
+
+                    pass
+
+            self.codigo_barras = f'CS{siguiente}'
+
+        super().save(*args, **kwargs)
+
     @property
     def ganancia(self):
-        return self.precio - self.precio_compra
+
+        return (
+            self.precio -
+            self.precio_compra
+        )
 
     @property
     def porcentaje_ganancia(self):
@@ -51,10 +94,18 @@ class Producto(models.Model):
             return 0
 
         return round(
-            ((self.precio - self.precio_compra)
-            / self.precio_compra) * 100,
+            (
+                (
+                    self.precio -
+                    self.precio_compra
+                )
+                /
+                self.precio_compra
+            ) * 100,
             2
         )
 
     def __str__(self):
+
         return self.nombre
+    
