2800d5b350e73a58ec5938350e5fe019381dad29
panapunker <ospisebas89@gmail.com>
2026-06-29 23:59:28 -0500

    Delete dashboard directory

--- dashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import reverse
-from django.utils.html import format_html
-
-
-admin.site.site_header = "Capital Sneakers"
-admin.site.site_title = "Capital Sneakers Admin"
-admin.site.index_title = "Administración"
