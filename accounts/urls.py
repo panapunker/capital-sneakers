a6b59c20058ef97e2fdaf1a5cc25cafcdbb5f9b7
panapunker <ospisebas89@gmail.com>
2026-06-29 23:58:11 -0500

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_cliente, name='login'),
-    path('logout/', views.logout_cliente, name='logout'),
-]
